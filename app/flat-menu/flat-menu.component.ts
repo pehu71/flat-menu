e88a1f53d8a188782e2373dc1950ebc60b5201f4
pehu71 <humplikpetr@gmail.com>
2017-04-01 11:33:02 +0200

    show-panel layout

--- app/flat-menu/flat-menu.component.ts
+++ app/flat-menu/flat-menu.component.ts
@@ -2,7 +2,23 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'flat-menu-vertical',
-    templateUrl: 'app/flat-menu/flat-menu-vertical.html'
+    template: `
+    <div class="fm-container">
+        <ul>
+            <li>fizz
+                <div class="show-panel">
+                    <ul>
+                        <li>sub-fizz-1</li>
+                        <li>sub-fizz-2</li>
+                        <li>sub-fizz-3</li>
+                    </ul>
+                </div>
+            </li>
+            <li>bauzz</li>
+            <li>bar</li>
+        </ul>
+    </div>
+    `
 })
 
 export class FlatMenuVertical {}
