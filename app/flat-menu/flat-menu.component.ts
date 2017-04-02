69d94fb6847d0f206707066ca425582b58241ad0
pehu71 <humplikpetr@gmail.com>
2017-04-02 13:07:07 +0200

    performance boost

--- app/flat-menu/flat-menu.component.ts
+++ app/flat-menu/flat-menu.component.ts
@@ -47,12 +47,13 @@ export class FlatMenuVertical implements OnInit {
 
     rootItemMouseEnter(index: number): void {
         this.hidePanels();
-
+        console.log('enter');
         let myClass: string = `div[class="show-panel sp-${index}"]`;
         $(myClass).attr('style', 'display:block')
     }
 
     rootMouseLeave(): void {
+        console.log('leave');
         this.hidePanels()
     }
 
