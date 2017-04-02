c7aafa8a2d8f910531b84eefb310549c976950ad
pehu71 <humplikpetr@gmail.com>
2017-04-02 11:55:59 +0200

    level 1 data-bound, showing/hiding done

--- app/flat-menu/flat-menu.component.ts
+++ app/flat-menu/flat-menu.component.ts
@@ -7,10 +7,16 @@ declare let $: any;
     template: `
         <div class="fm-container" (mouseleave)="rootMouseLeave($event)">
             <ul>
-                
-                <li *ngFor="let item of items; index as i;" (mouseenter)="rootMouseEnter($event, i)">{{item['label']}}
+
+                <li *ngFor="let item of items; index as i;" (mouseenter)="rootItemMouseEnter(i)">{{item['label']}}
                     <div class="show-panel {{'sp-' + i}}">
                         <div class="col-lg-12">
+
+                            <div *ngFor="let level2 of item.items; index as j;" class="col-lg-4">
+                                <h6>{{level2.label}}</h6>
+
+                            </div>
+                            
                         </div>
                     </div>
                 </li>
@@ -31,45 +37,55 @@ export class FlatMenuVertical implements OnInit {
             {
                 label: 'Electric guitars',
                 items: [{
-                    label: 'New',
+                    label: 'Stratocaster',
                     icon: 'fa-plus',
                     items: [
                         {label: 'Project'},
                         {label: 'Other'},
                     ]
                 },
-                    {label: 'Open'},
-                    {label: 'Quit'}
+                    {label: 'Les Paul'},
+                    {label: 'Flying-V'},
+                    {label: 'Mockingbird'},
+                    {label: 'Superstrat'}
                 ]
             },
             {
                 label: 'Acoustic guitars',
                 icon: 'fa-edit',
                 items: [
-                    {label: 'Undo', icon: 'fa-mail-forward'},
-                    {label: 'Redo', icon: 'fa-mail-reply'}
+                    {label: 'Jumbo', icon: 'fa-mail-forward'},
+                    {label: 'Blue Grass', icon: 'fa-mail-reply'},
+                    {label: 'For children'},
+                    {label: 'Signature models'}
                 ]
             },
             {
                 label: 'Accessories',
                 icon: 'fa-edit',
                 items: [
-                    {label: 'Undo', icon: 'fa-mail-forward'},
-                    {label: 'Redo', icon: 'fa-mail-reply'}
+                    {label: 'Strings', icon: 'fa-mail-forward'},
+                    {label: 'Pick-ups', icon: 'fa-mail-reply'},
+                    {label: 'Bridges'},
+                    {label: 'Tremolos'}
                 ]
             }
         ];
     }
 
-    rootMouseEnter(event: any, index: number): void {
+    rootItemMouseEnter(index: number): void {
+        this.hidePanels();
+
         let myClass: string = `div[class="show-panel sp-${index}"]`;
         $(myClass).attr('style', 'display:block')
-        // document.querySelector(myClass).setAttribute('style', 'display:block')
     }
 
-    rootMouseLeave(event: any, index: number): void {
+    rootMouseLeave(): void {
+        this.hidePanels()
+    }
+
+    private hidePanels(): void {
         $('.show-panel').attr('style', 'display:none')
-        // document.querySelector('div.show-panel').setAttribute('style', 'display:none')
     }
 
 }
