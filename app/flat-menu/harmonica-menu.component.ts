0327e673c4336e8c94b7cc506d0ceaecb87b28ff
pehu71 <humplikpetr@gmail.com>
2017-04-09 21:24:29 +0200

    up to level2

--- app/flat-menu/harmonica-menu.component.ts
+++ app/flat-menu/harmonica-menu.component.ts
@@ -7,14 +7,22 @@ import {MenuItem} from "./models";
 @Component({
     selector: 'harmonica-menu',
     template: `
-        <div class="fm-container">
-            <ul class="ul-level1" *ngIf="depth > 0">
+        <div class="fm-container-harmonica">
+            <ul class="har-level1" *ngIf="depth > 0">
                 
                 <li *ngFor="let item of items; index as i">
                     <a [routerLink]="item?.routerLink" id="{{'root_' + i}}">
                         <img *ngIf="item.image" class="menu-image" src="{{item.image}}">
                         {{item?.label}}
                     </a>
+                    
+                    <ul  class="har-level2" *ngIf="item?.items?.length > 0 && depth > 1">
+                        <li *ngFor="let level2 of item.items">
+                            <div>
+                                <a [routerLink]="level2?.routerLink">{{level2.label}}</a>
+                            </div>
+                        </li>
+                    </ul>
                     
                 </li>
             </ul>
